c95f78861fadce6f18b770f24c64277cca1763f4
MarkosAlt <est.mhernandez0301@uml.edu.ni>
2024-06-22 10:18:01 -0600

    Actualizacion de codigo

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import MenuLateral from './componets/MenuLateral'
+
+function App() {
+
+  return (
+    <>
+    <header/>
+    <main>
+      <aside>
+        <MenuLateral/>
+      </aside>
+      <section id="contenido">
+      <Routes>
+          <Route path="/" element={<h1>Inicio</h1>} />
+          <Route path="/productos" element={<h1>Productos</h1>} />
+          <Route path="/clientes" element={<h1>Clientes</h1>} />
+          <Route path="/ventas" element={<h1>Ventas</h1>} />        
+        </Routes>
+      </section>
+
+    </main>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/componets/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+    <div>Sistema de Gestion de ventas</div>
+    <img src="vite.svg" alt="js logo" />
+  </header>
+  )
+}
+
+export default Header
